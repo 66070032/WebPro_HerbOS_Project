2b9cf53326b9e888a049b1beccb199d2e12119aa
Jetnipat Tanamai <jokeped@Mac.it.kmitl.ac.th>
2025-02-06 18:51:36 +0700

    Fix

--- herbos_frontend/app/page.js
+++ herbos_frontend/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        let response = await fetch("https://web.api.jokeped.xyz:3100/");
+        let response = await fetch("https://web.api.jokeped.xyz/");
         const result = await response.json();
         setUsers(result);
       } catch (error) {
