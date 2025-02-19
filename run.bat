3a3d5c40118092003a2fa99d03dc9b5747350bde
Jetnipat Tanamai <jokeped@JokePed.local>
2025-02-19 10:46:58 +0700

    test

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-start cmd @cmd /k "cd herbos_backend && npm i && node herbos_backend/index.js"
+start cmd @cmd /k "cd herbos_backend && npm i && node index.js"
 start cmd @cmd /k "cd herbos_frontend && npm i && npm run dev"
