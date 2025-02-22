88b9433c4a6a8c9b78de382322bb73bc82dbee96
Jetnipat Tanamai <jokeped@JokePed.local>
2025-02-23 04:28:13 +0700

    Backend prepare for login/register

--- herbos_backend/index.js
+++ herbos_backend/index.js
@@ -1,90 +1,42 @@
 import express from 'express';
 import cors from 'cors';
-import mysql from 'mysql2/promise';
 import chalk from 'chalk';
-import cookieParser from 'cookie-parser';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
+import mysql from 'mysql2/promise';
 
-let config = process.env;
+const PORT = 3100;
 
-const app = new express();
+const pool = mysql.createPool({
+    host: 'db.jokeped.xyz',
+    user: 'herbos',
+    password: 'Scbm(UTNTbf!3KMw',
+    database: 'herbos'
+});
+
+const app = express();
 app.use(express.json());
-app.use(cookieParser());
-app.set("trust proxy", 1);
-app.use(cors(
-    {
-        origin: [
-            'https://web.jokeped.xyz',
-            'https://web.api.jokeped.xyz'
-        ],
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        credentials: true
-    }
-));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 
-const generateString = (length) => {
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    for (let i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
+app.get('/', (req, res) => {
+    res.send("HerbOS Backend API");
+});
+
+try {
+    console.clear();
+    await pool.getConnection();
+    console.log(`${chalk.blue('[INFO]')} [${new Date().toLocaleString()}] Database connected`);
+    app.listen(3100, () => {
+        console.log(`${chalk.blue('[INFO]')} [${new Date().toLocaleString()}] Server is running on port ${chalk.greenBright('3000')}`);
+        pool.releaseConnection();
+    })
+} catch (error) {
+    console.log(chalk.red(error));
+    process.exit(1);
 }
-
-const validateEmail = (email) => {
-    return email.match(
-      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-};
-
-const delay = async (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-let db;
-async function connection() {
-    try {
-        db = await mysql.createConnection({
-            host: 'jokeped.xyz',
-            port: 3306,
-            user: 'funweb',
-            password: 'funweb123!',
-            database: 'funweb'
-        });
-
-        await delay(500);
-        console.clear();
-        console.log(`${chalk.blue('[INFO]')} Database connect ${chalk.green('successfully')}`);
-
-        app.listen(3100, () => {
-            console.log(`${chalk.blue('[INFO]')} API Server locate at ${chalk.green(`http://localhost:${3100}`)}`);
-        });
-
-        return db;
-    } catch (error) {
-        console.log(error);
-    }
-}
-connection();
-
-app.get('/', async (req, res) => {
-    const [row, fields] = await db.execute('SELECT * FROM users');
-    console.log("Enter.")
-    res.send(row);
-});
-
-app.get("/set-cookie", (req, res) => {
-    res.cookie("username", "JohnDoe", { maxAge: 900000, httpOnly: true, secure: true, sameSite: "None", });
-    res.send("Cookie ถูกตั้งค่าแล้ว");
-});
-
-app.get("/get-cookie", (req, res) => {
-    const username = req.cookies.username;
-    res.send(`ค่าของ cookie: ${username}`);
-});
-
-app.get("/delete-cookie", (req, res) => {
-    res.clearCookie("username");
-    res.send("Cookie ถูกลบแล้ว");
-});
