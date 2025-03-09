c73938867ed16ebdcff75a36cfececa6e8ad93dc
fordntp <141384976+66070067@users.noreply.github.com>
2025-03-10 03:05:07 +0700

    added loopbar

---
+++ herbos_frontend/components/LoopBar.jsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+const LoopBar = () => {
+    return (
+      <div className="w-full bg-violet-200 overflow-hidden">
+        <div className="p-8">          
+        </div>
+        <div className="flex space-x-4 animate-scroll">
+          {Array.from({ length: 360 }).map((_, index) => (
+            <div key={`clone-${index}`} className="p-2 flex items-center whitespace-nowrap">
+              <span className="text-lg font-bold mr-10">สมุนไพรไทยแท้ เพื่อสุขภาพที่ดีกว่า</span>
+              <Image src="/blink.svg" width={20} height={10} alt="blink" />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+};
+
+export default LoopBar;
