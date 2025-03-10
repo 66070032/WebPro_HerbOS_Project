9860826f70780c421841142239076c84a8fd958d
Jetnipat Tanamai <jokeped@JokePed.local>
2025-03-10 15:00:36 +0700

    Orders from desc to asc

--- herbos_backend/index.js
+++ herbos_backend/index.js
@@ -397,7 +397,7 @@ app.get("/orders", verifyToken, async (req, res) => {
         orders.total_amount
       FROM orders
       LEFT JOIN users ON orders.user_id = users.id
-      ORDER BY orders.order_id DESC
+      ORDER BY orders.order_id ASC
     `,
       [req.user.userId]
     );
